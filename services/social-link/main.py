d3e26330876db8bb5d22601b54ff8f78fa34b9d0
ianmartin16 <imartindm20@gmail.com>
2026-03-19 14:28:11 -0600

    Add cors features

--- services/social-link/main.py
+++ services/social-link/main.py
@@ -53,7 +53,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://www.cryptolink.mx",
+        "https://cryptolink.mx",
     ],
     allow_credentials=True,
     allow_methods=["*"],
